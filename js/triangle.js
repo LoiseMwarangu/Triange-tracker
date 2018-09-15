76ce746fef59b685278fcb155a9fe674544160ca
Loise Mwarangu <loisemwarangu@gmail.com>
2018-09-16 01:04:06 +0300

    linked css and js files to html file

--- js/triangle.js
+++ js/triangle.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
-      $("#blanks form").submit(function() {
-        var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+alert("Please read through the instructions carefully to avoid errors");
+var side1=parseInt(side1)
+var side2=parseInt(side2)
+var side3=parseInt(side3)
+(document).ready(function() {
+("#types form").submit(function(event) {
+var types  =["side1", "side2", "side3"];
+types.for(function(type) {
+if (" .index[0] === .index[1] " && " .index[1] === .index[2] "  && " .index[0] === .index[2] "){
+alert("This is an equilateral triangle")}
+else if (" .index[0] !== .index[1] " && " .index[1] != .index[2] " && " .index[0] !== .index[2]") {
+alert("This is a scalene triangle")
+}
+else if () {
 
-        blanks.forEach(function(blank) {
-          var userInput = $("input." + blank).val();
-          $("." + blank).text(userInput).val();
-        });
+}
 
-        $("#story").sho();
-      });
-    });
+}
+  });
