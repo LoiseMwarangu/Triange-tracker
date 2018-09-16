c2b1ee7ab557ee1d382bbb606ec205160ee3339a
Loise Mwarangu <loisemwarangu@gmail.com>
2018-09-16 13:26:10 +0300

    created a file named license

---
+++ js/triangle.js
@@ -0,0 +1,16 @@
+alert("Please read through the instructions carefully to avoid errors");
+(document).ready(function() {
+("#types form").submit(function(event) {
+var types  =["side1", "side2", "side3"];
+types.for(function(type) {
+if (" .index[0] === .index[1] " && " .index[1] === .index[2] "  && " .index[0] === .index[2] "){
+alert("This is an equilateral triangle")}
+else if (" .index[0] !== .index[1] " && " .index[1] != .index[2] " && " .index[0] !== .index[2]") {
+alert("This is a scalene triangle")
+}
+else if () {
+
+}
+
+}
+  });
