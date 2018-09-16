2ae7dd4f7bebe55d95cde13fa6be3559468b2cc9
Loise Mwarangu <loisemwarangu@gmail.com>
2018-09-16 20:18:28 +0300

    removed forms and replaced with prompts

--- js/triangle.js
+++ js/triangle.js
@@ -1,9 +1,9 @@
 alert("Please read through the instructions carefully to avoid errors");
 
-(document).ready(function() {
+(document).ready(function(types) {
 ("#types form").submit(function(event) {
 var types  =["side1", "side2", "side3"];
-types.for(function(type) {
+types.forEach(function() {
 if (" .index[0] === .index[1] " && " .index[1] === .index[2] "  && " .index[0] === .index[2] "){
 alert("This is an equilateral triangle")}
 else if (" .index[0] !== .index[1] " && " .index[1] != .index[2] " && " .index[0] !== .index[2]") {
